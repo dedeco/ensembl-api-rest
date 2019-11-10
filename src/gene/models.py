a8eae26aa1a78165cf9224788beb90dad62183e6
André de Sousa Araújo <dedeco@gmail.com>
2019-11-10 00:47:24 -0300

    We now have a Gene model

---
+++ src/gene/models.py
@@ -0,0 +1,18 @@
+from src import db
+
+
+class Gene(db.Model):
+    __tablename__ = 'gene_autocomplete'
+
+    stable_id = db.Column(db.String(128), primary_key=True)
+    species = db.Column(db.String(255))
+    display_label = db.Column(db.String(128))
+    location = db.Column(db.String(60))
+    db = db.Column(db.String(32), default="core", nullable=False)
+
+    def __init__(self, stable_id, species, display_label, location, db_):
+        self.stable_id = stable_id
+        self.species = species
+        self.display_label = display_label
+        self.location = location
+        self.db = db_
---
+++ tests/unit/gene_test.py
@@ -0,0 +1,29 @@
+import unittest
+
+from src.gene.models import Gene
+
+
+class GeneCase(unittest.TestCase):
+    def setUp(self):
+        new_gene = Gene("id",
+                        "species 1",
+                        "name 1",
+                        "location 1",
+                        "core")
+
+        self.test_gene = new_gene
+
+    def test_create_gene(self):
+        self.assertIsInstance(self.test_gene, Gene)
+
+    def test_new_gene(self):
+
+        self.assertTrue(self.test_gene.stable_id == "id")
+        self.assertTrue(self.test_gene.species == "species 1")
+        self.assertTrue(self.test_gene.display_label == "name 1")
+        self.assertTrue(self.test_gene.location == "location 1")
+        self.assertTrue(self.test_gene.db == "core")
+
+
+if __name__ == '__main__':
+    unittest.main()
