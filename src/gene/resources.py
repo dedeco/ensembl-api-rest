e7a44aad37c7f9f630695bfeda4465ad62ce73e3
André de Sousa Araújo <dedeco@gmail.com>
2019-11-16 18:31:37 -0300

    Added some documention to endpoint

--- src/gene/resources.py
+++ src/gene/resources.py
@@ -16,6 +16,44 @@ class GenesResource(Resource):
 
     @cache.cached(key_prefix=make_cache_key)
     def get(self):
+        """
+        Get the genes. The entry point accepts a string as parameter and
+        return the list of matching genes in our database.
+
+        Parameters
+        ----------
+        lookup : str
+            display_name - the name of the gene. The partial query typed
+            by the user, e.g. brc
+        species : str
+            species - the name of the species to which the gene belongs.
+            The name of the target species, e.g. homo_sapiens (not mandatory)
+
+        Returns
+        -------
+        The server should return something like this:
+         {
+             "start": 1,
+             "limit": 3,
+             "count": 4,
+             "previous": "",
+             "next": "/api/v1/genes?start=4&limit=2&lookup=brc&species=sapiens",
+             "results": [
+                 {
+                     "species": "homo_sapiens",
+                     "ensembl_stable_id": "ENSG00000012048",
+                     "gene_name": "BRCA1",
+                     "location": "17:43044295-43170245"
+                 },
+                 {
+                     "species": "homo_sapiens",
+                     "ensembl_stable_id": "ENSG00000185515",
+                     "gene_name": "BRCC3",
+                     "location": "X:155071420-155123074"
+                 }
+             ]
+         }
+        """
 
         args = get_parsed_parameters(reqparse.RequestParser())
 
